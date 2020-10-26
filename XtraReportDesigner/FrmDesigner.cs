a633cbae5fc0b03ceb720495d5039bd115e48e03
İlker Doğan <ilkerdogan@outlook.com>
2020-10-26 13:22:47 +0300

    Add project files.

---
+++ XtraReportDesigner/FrmDesigner.cs
@@ -0,0 +1,14 @@
+﻿
+namespace XtraReportDesigner
+{
+    public partial class FrmDesigner : DevExpress.XtraEditors.XtraForm
+    {
+        public FrmDesigner(string filePath)
+        {
+            InitializeComponent();
+
+            if (!string.IsNullOrEmpty(filePath))
+                reportDesigner1.OpenReport(filePath);
+        }
+    }
+}
---
+++ XtraReportDesigner/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+using DevExpress.LookAndFeel;
+using DevExpress.Skins;
+using DevExpress.UserSkins;
+
+namespace XtraReportDesigner
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            BonusSkins.Register();
+            SkinManager.EnableFormSkins();
+            UserLookAndFeel.Default.SetSkinStyle("DevExpress Style");
+
+            string filePath = string.Empty;
+            if (args.Length > 0)
+            {
+                filePath = args[0];
+            }
+
+            Application.Run(new FrmDesigner(filePath));
+        }
+    }
+}
